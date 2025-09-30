596f1ec60ffe596075cd197b7de5935ac4e3eac3
WalissonCF <walissoncf44@gmail.com>
2025-09-30 20:26:31 -0300

    feat: enhance home page SEO and accessibility

--- playground/showcase/src/app/pages/home/home.ts
+++ playground/showcase/src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'wally-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
