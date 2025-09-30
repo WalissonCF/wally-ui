01b1dbd2fc75941fe486d7e9461e0473057464d6
WalissonCF <walissoncf44@gmail.com>
2025-09-30 20:48:53 -0300

    feat: enhance documentation

--- playground/showcase/src/app/pages/documentation/documentation.ts
+++ playground/showcase/src/app/pages/documentation/documentation.ts
@@ -1,10 +1,14 @@
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 import { Breadcrumb, BreadcrumbItem } from '../../components/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'wally-documentation',
-  imports: [Breadcrumb],
+  imports: [
+    Breadcrumb,
+    RouterModule
+  ],
   templateUrl: './documentation.html',
   styleUrl: './documentation.css'
 })
